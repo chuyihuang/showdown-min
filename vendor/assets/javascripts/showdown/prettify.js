edd4c384a8339c161ca6ec020deadfc93f3039b4
chuyihuang <chuyihuang@gmail.com>
2013-08-13 11:41:36 +0800

    fix minified files

--- vendor/assets/javascripts/showdown/prettify.js
+++ vendor/assets/javascripts/showdown/prettify.js
@@ -1,13 +1,1 @@
-(function() {
-    var a = function(a) {
-        return [{
-            type: "output",
-            filter: function(a) {
-                return a.replace(/(<pre>)?<code>/gi, function(a, b) {
-                    return b ? '<pre class="prettyprint linenums" tabIndex="0"><code data-inner="1">' : '<code class="prettyprint">'
-                })
-            }
-        }]
-    };
-    typeof window != "undefined" && window.Showdown && window.Showdown.extensions && (window.Showdown.extensions.prettify = a), typeof module != "undefined" && (module.exports = a)
-})();
+(function(){var a=function(a){return[{type:"output",filter:function(a){return a.replace(/(<pre>)?<code>/gi,function(a,b){return b?'<pre class="prettyprint linenums" tabIndex="0"><code data-inner="1">':'<code class="prettyprint">'})}}]};typeof window!="undefined"&&window.Showdown&&window.Showdown.extensions&&(window.Showdown.extensions.prettify=a),typeof module!="undefined"&&(module.exports=a)})();
